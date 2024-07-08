b95c4ca7bd52d9a0ffda3deda7fdb5bb924d981b
Wh1isper <9573586@qq.com>
2024-07-08 11:06:15 +0800

    Add test code base for autoscaler

---
+++ tests/matrix/operator_/autoscale/test_autoscaler.py
@@ -0,0 +1,25 @@
+import pytest
+
+from moriarty.matrix.operator_.autoscaler import AutoscalerManager
+
+
+@pytest.fixturea
+async def autoscaler(
+    async_redis_client,
+    async_session,
+    spawner,
+):
+    return AutoscalerManager(
+        redis_client=async_redis_client, session=async_session, spawner=spawner
+    )
+
+
+@pytest.fixture
+def scaleable_endpoint(session):
+    endpoint_name = "mock_scaleable_endpoint"
+
+    yield endpoint_name
+
+
+async def test_scan_and_scale(autoscaler, scaleable_endpoint):
+    pass
