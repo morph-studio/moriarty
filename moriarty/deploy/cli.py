d2315c8f5abca0062325e80ace9dcbd927ea22bd
Wh1isper <9573586@qq.com>
2024-07-10 16:46:06 +0800

    Imp some sdk

--- moriarty/deploy/cli.py
+++ moriarty/deploy/cli.py
@@ -1,8 +1,17 @@
 import click
 
 from moriarty.envs import MORIARTY_MATRIX_API_URL_ENV, MORIARTY_MATRIX_TOKEN_ENV
+from moriarty.matrix.operator_.enums_ import MetricType
 
-from .sdk import delete_endpoint, query_endpoint, scan_endpoint
+from .sdk import (
+    request_delete_autoscale,
+    request_delete_endpoint,
+    request_query_autoscale,
+    request_query_endpoint,
+    request_scan_autoscale_log,
+    request_scan_endpoint,
+    request_set_autoscale,
+)
 
 
 @click.command()
@@ -60,7 +69,7 @@ def scan(
 ):
     cursor = cursor
     while True:
-        response = scan_endpoint(
+        response = request_scan_endpoint(
             api_url=api_url,
             limit=limit,
             cursor=cursor,
@@ -76,9 +85,13 @@ def scan(
         cursor = response.cursor
 
         for endpoint in response.endpoints:
-            print(endpoint)
+            autoscale = request_query_autoscale(
+                endpoint_name=endpoint.endpoint_name, api_url=api_url, token=token
+            )
+            print(f"Endpoint: {endpoint} | Autoscale: {autoscale}")
 
         if not response.endpoints:
+            print("\nNo more results...")
             return
         input("Press Enter to continue...")
 
@@ -103,12 +116,18 @@ def query(
     api_url,
     token,
 ):
-    response = query_endpoint(
+    response = request_query_endpoint(
         endpoint_name=endpoint_name,
         api_url=api_url,
         token=token,
     )
     print(response)
+
+    autoscale = request_query_autoscale(endpoint_name=endpoint_name, api_url=api_url, token=token)
+    if autoscale:
+        print(autoscale)
+    else:
+        print(f"No autoscale found for {endpoint_name}")
 
 
 @click.command()
@@ -173,10 +192,49 @@ def delete(
     api_url,
     token,
 ):
-    delete_endpoint(endpoint_name=endpoint_name, api_url=api_url, token=token)
+    request_delete_endpoint(endpoint_name=endpoint_name, api_url=api_url, token=token)
+    print(f"Endpoint {endpoint_name} deleted")
 
 
 @click.command()
+@click.argument("endpoint_name")
+@click.option(
+    "--min-replicas",
+    required=True,
+    help="Minimum number of replicas",
+    type=int,
+)
+@click.option(
+    "--max-replicas",
+    required=True,
+    help="Minimum number of replicas",
+    type=int,
+)
+@click.option(
+    "--scale-in-cooldown",
+    required=True,
+    help="Scale in cooldown",
+    type=int,
+)
+@click.option(
+    "--scale-out-cooldown",
+    required=True,
+    help="Scale out cooldown",
+    type=int,
+)
+@click.option(
+    "--metrics",
+    required=True,
+    default=MetricType.pending_jobs_per_instance.value,
+    help="Metrics",
+    type=click.Choice(MetricType.__members__.keys()),
+)
+@click.option(
+    "--metrics-threshold",
+    required=True,
+    help="Metrics threshold",
+    type=int,
+)
 @click.option(
     "--api-url",
     help="Moriarty Operator API URL",
@@ -191,13 +249,38 @@ def delete(
     envvar=MORIARTY_MATRIX_TOKEN_ENV,
 )
 def autoscale(
+    endpoint_name,
+    min_replicas,
+    max_replicas,
+    scale_in_cooldown,
+    scale_out_cooldown,
+    metrics,
+    metrics_threshold,
     api_url,
     token,
 ):
-    pass
+    request_set_autoscale(
+        endpoint_name=endpoint_name,
+        min_replicas=min_replicas,
+        max_replicas=max_replicas,
+        scale_in_cooldown=scale_in_cooldown,
+        scale_out_cooldown=scale_out_cooldown,
+        metrics=metrics,
+        metrics_threshold=metrics_threshold,
+        api_url=api_url,
+        token=token,
+    )
+
+    autoscale = request_query_autoscale(endpoint_name=endpoint_name, api_url=api_url, token=token)
+    if autoscale:
+        print("Autoscale set successfully")
+        print(autoscale)
+    else:
+        print(f"No autoscale found for {endpoint_name}")
 
 
 @click.command()
+@click.argument("endpoint_name")
 @click.option(
     "--api-url",
     help="Moriarty Operator API URL",
@@ -212,13 +295,58 @@ def autoscale(
     envvar=MORIARTY_MATRIX_TOKEN_ENV,
 )
 def delete_autoscale(
+    endpoint_name,
     api_url,
     token,
 ):
-    pass
+    request_query_endpoint(
+        endpoint_name=endpoint_name,
+        api_url=api_url,
+        token=token,
+    )
+
+    request_delete_autoscale(endpoint_name=endpoint_name, api_url=api_url, token=token)
+    print(f"Autoscale for {endpoint_name} deleted")
+    response = request_query_endpoint(
+        endpoint_name=endpoint_name,
+        api_url=api_url,
+        token=token,
+    )
+    print(response)
 
 
 @click.command()
+@click.argument("endpoint_name")
+@click.option(
+    "--limit",
+    default=10,
+    help="Limit number of results",
+    type=int,
+)
+@click.option(
+    "--cursor",
+    default=None,
+    help="Cursor",
+    type=int,
+)
+@click.option(
+    "--keyword",
+    default=None,
+    help="Keyword",
+    type=str,
+)
+@click.option(
+    "--order_by",
+    default="created_at",
+    help="Order by",
+    type=str,
+)
+@click.option(
+    "--order",
+    default="desc",
+    help="Order",
+    type=str,
+)
 @click.option(
     "--api-url",
     help="Moriarty Operator API URL",
@@ -232,11 +360,39 @@ def delete_autoscale(
     type=str,
     envvar=MORIARTY_MATRIX_TOKEN_ENV,
 )
-def query_log(
+def query_autoscale_log(
+    endpoint_name,
     api_url,
     token,
+    limit,
+    cursor,
+    keyword,
+    order_by,
+    order,
 ):
-    pass
+    cursor = cursor
+    while True:
+        response = request_scan_autoscale_log(
+            api_url=api_url,
+            endpoint_name=endpoint_name,
+            limit=limit,
+            cursor=cursor,
+            keyword=keyword,
+            order_by=order_by,
+            order=order,
+            token=token,
+        )
+
+        print(f"Count: {len(response.logs)} | Total: {response.total} [Cursor: {response.cursor}]")
+        cursor = response.cursor
+
+        for log in response.logs:
+            print(log)
+
+        if not response.logs:
+            print("\nNo more results...")
+            return
+        input("Press Enter to continue...")
 
 
 @click.group()
@@ -251,4 +407,4 @@ cli.add_command(update)
 cli.add_command(delete)
 cli.add_command(autoscale)
 cli.add_command(delete_autoscale)
-cli.add_command(query_log)
+cli.add_command(query_autoscale_log)
--- moriarty/deploy/sdk.py
+++ moriarty/deploy/sdk.py
@@ -3,9 +3,13 @@ from urllib.parse import urljoin
 import httpx
 
 from moriarty.log import logger
+from moriarty.matrix.operator_.enums_ import MetricType
 from moriarty.matrix.operator_.params import (
     ListEndpointsResponse,
+    QueryEndpointAutoscaleLogResponse,
+    QueryEndpointAutoscaleResponse,
     QueryEndpointResponse,
+    SetAutoscaleParams,
 )
 
 
@@ -13,7 +17,7 @@ def _make_headers(token: str | None) -> dict:
     return {"Authorization": f"Bearer {token}"} if token else dict()
 
 
-def scan_endpoint(
+def request_scan_endpoint(
     api_url: str,
     limit: int = None,
     cursor: str = None,
@@ -25,11 +29,15 @@ def scan_endpoint(
     response = httpx.get(
         urljoin(api_url, "/endpoint/list"),
         params={
-            "limit": limit,
-            "cursor": cursor,
-            "keyword": keyword,
-            "order_by": order_by,
-            "order": order,
+            k: v
+            for k, v in {
+                "limit": limit,
+                "cursor": cursor,
+                "keyword": keyword,
+                "order_by": order_by,
+                "order": order,
+            }.items()
+            if v is not None
         },
         headers=_make_headers(token),
         follow_redirects=True,
@@ -42,7 +50,41 @@ def scan_endpoint(
     return ListEndpointsResponse.model_validate(response.json())
 
 
-def query_endpoint(
+def request_scan_autoscale_log(
+    api_url: str,
+    endpoint_name: str,
+    limit: int = None,
+    cursor: int = None,
+    keyword: str = None,
+    order_by: str = None,
+    order: str = None,
+    token: str = None,
+) -> QueryEndpointAutoscaleLogResponse:
+    response = httpx.get(
+        urljoin(api_url, f"/autoscale/{endpoint_name}/log"),
+        params={
+            k: v
+            for k, v in {
+                "limit": limit,
+                "cursor": cursor,
+                "keyword": keyword,
+                "order_by": order_by,
+                "order": order,
+            }.items()
+            if v is not None
+        },
+        headers=_make_headers(token),
+        follow_redirects=True,
+    )
+    try:
+        response.raise_for_status()
+    except httpx.HTTPStatusError:
+        logger.error(f"Request error, response text: {response.text}")
+        raise
+    return QueryEndpointAutoscaleLogResponse.model_validate(response.json())
+
+
+def request_query_endpoint(
     endpoint_name: str,
     api_url: str,
     token: str = None,
@@ -60,7 +102,7 @@ def query_endpoint(
     return QueryEndpointResponse.model_validate(response.json())
 
 
-def delete_endpoint(
+def request_delete_endpoint(
     endpoint_name: str,
     api_url: str,
     token: str = None,
@@ -73,7 +115,85 @@ def delete_endpoint(
     try:
         response.raise_for_status()
     except httpx.HTTPStatusError:
+        if response.status_code == 404:
+            return None
+
         logger.error(f"Request error, response: {response.text}")
         raise
 
     return None
+
+
+def request_delete_autoscale(
+    endpoint_name: str,
+    api_url: str,
+    token: str = None,
+) -> None:
+    response = httpx.post(
+        urljoin(api_url, f"/autoscale/{endpoint_name}/delete"),
+        headers=_make_headers(token),
+        follow_redirects=True,
+    )
+    try:
+        response.raise_for_status()
+    except httpx.HTTPStatusError:
+        if response.status_code == 404:
+            return None
+
+        logger.error(f"Request error, response: {response.text}")
+        raise
+
+    return None
+
+
+def request_query_autoscale(
+    endpoint_name: str,
+    api_url: str,
+    token: str = None,
+) -> QueryEndpointAutoscaleResponse | None:
+    response = httpx.get(
+        urljoin(api_url, f"/endpoint/{endpoint_name}/autoscale"),
+        headers=_make_headers(token),
+        follow_redirects=True,
+    )
+    try:
+        response.raise_for_status()
+    except httpx.HTTPStatusError as e:
+        if e.response.status_code == 404:
+            return None
+
+        logger.error(f"Request error, response: {response.text}")
+        raise
+    return QueryEndpointAutoscaleResponse.model_validate(response.json())
+
+
+def request_set_autoscale(
+    endpoint_name: str,
+    api_url: str,
+    min_replicas: int,
+    max_replicas: int,
+    scale_in_cooldown: int,
+    scale_out_cooldown: int,
+    metrics: MetricType,
+    metrics_threshold: float,
+    token: str = None,
+) -> None:
+    response = httpx.post(
+        urljoin(api_url, f"/autoscale/{endpoint_name}/set"),
+        headers=_make_headers(token),
+        data=SetAutoscaleParams(
+            min_replicas=min_replicas,
+            max_replicas=max_replicas,
+            scale_in_cooldown=scale_in_cooldown,
+            scale_out_cooldown=scale_out_cooldown,
+            metrics=metrics,
+            metrics_threshold=metrics_threshold,
+        ).model_dump_json(),
+        follow_redirects=True,
+    )
+    try:
+        response.raise_for_status()
+    except httpx.HTTPStatusError:
+        logger.error(f"Request error, response: {response.text}")
+        raise
+    return QueryEndpointResponse.model_validate(response.json())
