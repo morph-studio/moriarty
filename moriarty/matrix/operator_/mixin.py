a571df3f2412c96c1ae8e6e1fc8162bb938316be
Wh1isper <9573586@qq.com>
2024-08-20 18:18:18 +0800

    Fix calculate least replicas

--- moriarty/matrix/operator_/mixin.py
+++ moriarty/matrix/operator_/mixin.py
@@ -48,6 +48,7 @@ class MetricsManager:
         metric: MetricType,
         current_metric_value: float | int,
         metric_threshold: float,
+        current_instance_value: int,
     ) -> int:
         logger.debug(
             f"Calculating least replicas:`{metric}`(current `{current_metric_value}` threshold `{metric_threshold}`)"
@@ -57,7 +58,7 @@ class MetricsManager:
         if metric == MetricType.pending_jobs_per_instance:
             if metric_threshold == 0:
                 metric_threshold = 1
-            return math.ceil(current_metric_value / metric_threshold)
+            return math.ceil(current_metric_value * current_instance_value / metric_threshold)
 
         raise NotImplementedError
 
